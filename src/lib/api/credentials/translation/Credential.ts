b7d983afcf3c38f5ea79179b39966ab022dd5128
AM_Trustup <axelismv4630@gmail.com>
2023-05-04 11:23:43 +0200

    :sparkles: Now call the translation file

--- src/lib/api/credentials/translation/Credential.ts
+++ src/lib/api/credentials/translation/Credential.ts
@@ -1,14 +1,17 @@
-import { JsonCredential, Request } from "@henrotaym/api-client";
+import {
+  Request,
+  Credential as PackageCredential,
+} from "@henrotaym/api-client";
 
-class Credential extends JsonCredential {
+class Credential extends PackageCredential {
   /**
    * Preparing given request.
    * @param {import('@henrotaym/api-client').Request} request
    */
 
-  prepare(request: Request<any>): void {
-    super.prepare(request);
+  prepare(request: Request): void {
     request.setBaseUrl("https://translations.trustup.io");
+    request.addHeaders({ Accept: "application/json" });
   }
 }
 
