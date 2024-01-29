ba6ddbcd31070df7ecc525f39b1fc60a19af3458
AM_Trustup <axelismv4630@gmail.com>
2024-01-29 16:13:51 +0100

    :sparkles: Add script that save in public folder translation file

--- src/lib/api/endpoints/translation/Translation.ts
+++ src/lib/api/endpoints/translation/Translation.ts
@@ -1,6 +1,9 @@
 import { Client, Request } from "@henrotaym/api-client";
 import { TranslationFileCredential } from "../../credentials";
 
+const fs = require('fs').promises;
+const path = require('path');
+
 class Translation {
   private client: Client;
 
@@ -14,9 +17,33 @@ class Translation {
       .setUrl(`storage/translations/${appName}.json`);
     const response = await this.client.try(request);
 
-    if (response.failed()) return;
+    if (response.failed()){
+      return this.readJsonFile(appName)
+            .then((jsonData) => {
+              return jsonData;
+            })
+            .catch((error) => {
+              return;
+            });
+    };
 
     return response.get();
+  }
+
+  private async readJsonFile(appName : string) {
+    try {
+      // Spécifiez le chemin vers votre fichier JSON à partir de la racine du projet
+      const filePath = path.join(process.cwd(), 'public', `${appName}.json`);
+  
+      // Lisez le fichier JSON de manière asynchrone
+      const data = await fs.readFile(filePath, 'utf-8');
+      
+      // Retournez le contenu JSON
+      return JSON.parse(data);
+    } catch (error) {
+      // En cas d'erreur, rejetez la promesse avec l'erreur
+      throw new Error('Erreur lors de la lecture du fichier JSON : ');
+    }
   }
 }
 export default Translation;
